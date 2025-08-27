0865a2213f68d003d23f3cee81d905bc2ceb1493
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2025-08-27 12:33:25 -0700

    change officers page layout

--- components/officer-bio.tsx
+++ components/officer-bio.tsx
@@ -1,10 +1,30 @@
 import styles from "@/styles/officer-bio.module.css"
 import Image from "next/image";
 
-const OfficerBio = ({ children, imageSrc, marginBottom }: { children: React.ReactNode, imageSrc: string, marginBottom: string|undefined }) => {
+export const OfficersWrapper = ({ children }: { children: React.ReactNode }) => {
+
+  const wrapperStyles: React.CSSProperties = {
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: "24px",
+    width: "90vw",
+    position: "relative",
+    left: "50%",
+    transform: "translateX(-50%)",
+    justifyContent: "space-between"
+  }
   
   return (
-    <div className={styles.bioWrapper} style={{marginBottom: marginBottom}}>
+    <div style={wrapperStyles}>
+      {children}
+    </div>
+  )
+}
+
+const OfficerBio = ({ children, imageSrc }: { children: React.ReactNode, imageSrc: string }) => {
+  
+  return (
+    <div className={styles.bioWrapper}>
       <Image src={imageSrc} alt="" height={350} width={350} />
       <div>
         {children}
