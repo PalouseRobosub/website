f28e54cfeeb148d73a42a42887c398070779cf18
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2025-08-28 00:37:09 -0700

    custom checkbox in mdx components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,21 @@
-import type { MDXComponents } from 'mdx/types'
- 
+import type { MDXComponents } from "mdx/types";
+
 const components: MDXComponents = {
-  wrapper: ({ children }) => <main>{ children }</main>
-}
- 
+  wrapper: ({ children }) => <main>{children}</main>,
+  input: (props) => {
+    if (props.type === "checkbox" && props.disabled) {
+      return (
+        <input
+          type="checkbox"
+          defaultChecked={props.checked}
+          style={{ pointerEvents: "none" }}
+        />
+      );
+    }
+    return <input {...props} />;
+  },
+};
+
 export function useMDXComponents(): MDXComponents {
-  return components
+  return components;
 }
