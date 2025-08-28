e0bf12fcc45c3b9f1f3bbedc4a51889cd916fc36
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2025-08-28 00:37:44 -0700

    add dynamic post routing

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,20 @@
+import { readdirSync } from "fs"
+import { join } from "path"
+
+export const dynamicParams = false
+
+export function generateStaticParams() {
+  const posts = readdirSync(join(process.cwd(), "/blog"))
+  
+  const slugs = posts.map((post) => ({
+    slug: post.split(".")[0]
+  }))
+ return slugs
+}
+
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params
+  const { default: Post } = await import(`@/blog/${slug}.mdx`)
+ 
+  return <Post />
+}
