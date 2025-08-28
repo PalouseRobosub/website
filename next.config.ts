db85f6f774b23fb4fe7f4d45ae6771f6df4a96a0
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2025-08-28 00:37:27 -0700

    add markdown processing plugins

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,10 @@ const nextConfig: NextConfig = {
 
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
+  options: {
+    remarkPlugins: ["remark-gfm", "remark-prism"],
+    rehypePlugins: ["rehype-slug", "rehype-autolink-headings"]
+  }
 })
 
 export default withMDX(nextConfig);
