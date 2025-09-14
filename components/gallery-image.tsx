3a2740174b6aee9a3938351905fc78c1fd699373
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2025-09-14 01:28:48 -0700

    suppress eslint warning

--- components/gallery-image.tsx
+++ components/gallery-image.tsx
@@ -4,6 +4,7 @@ const GalleryImage = ({ name, caption }: { name: string, caption: string }) => {
   
   return (
     <div className={styles.container}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img src={`gallery-images/${name}`} alt="" className={styles.image} />
       <div className={styles.caption}>{caption}</div>
     </div>
